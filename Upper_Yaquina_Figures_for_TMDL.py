3da2e4dd772f01918b24a80f6dd8cd9741349cea
Creutzx <creutzburg.brian@deq.state.or.us>
2018-01-09 12:19:08 -0800

    Exports PNG but extent is entire state

--- Upper_Yaquina_Figures_for_TMDL.py
+++ Upper_Yaquina_Figures_for_TMDL.py
@@ -6,7 +6,7 @@ import arcpy
 chr_dir_work = r'\\DEQHQ1\TMDL_WR\MidCoast\GIS\Figures\python\makelocinstate'
 chr_map_doc_base = r'\\DEQHQ1\TMDL\TMDL_WR\MidCoast\GIS\Figures\Upper_Yaquina_Maps\Upper_Yaquina_TMDL_BC_Automated_V2.mxd'
 chr_map_doc_new = r'\\DEQHQ1\TMDL\TMDL_WR\MidCoast\GIS\Figures\python\makelocinstate\Upper_Yaquina_KMB.mxd'
-chr_png_map_new = r'\\DEQHQ1\TMDL_WR\MidCoast\GIS\Figures\Upper_Yaquina_Maps\Up_Yaq.png'
+chr_png_map_new = r'\\DEQHQ1\TMDL\TMDL_WR\MidCoast\GIS\Figures\Upper_Yaquina_Maps\Up_Yaq.png'
 chr_lyr_name_sel = r'HUC 10'
 chr_lyr_sym_wtsd = r'\\DEQHQ1\TMDL\TMDL_WR\MidCoast\GIS\Layer\upper_yaquina_official_huc_10.lyr'
 chr_lyr_sym_state = r'\\DEQHQ1\TMDL\TMDL_WR\MidCoast\GIS\Figures\Upper_Yaquina_Maps\or_state_boundary_for_inset.lyr'
@@ -48,14 +48,14 @@ lyr_wtsd = arcpy.mapping.ListLayers(mxd_new, chr_lyr_name_sel, df)[0]
 # get the watershed boundary and state layers that have the symbology to be used
 lyr_sym_wtsd = arcpy.mapping.Layer(chr_lyr_sym_wtsd)
 lyr_sym_state = arcpy.mapping.Layer(chr_lyr_sym_state)
-# change the symbology of the watershed boundary in map doc to that of the symbology of the layer file
+# change the symbology of the watershed boundary in magit p doc to that of the symbology of the layer file
 arcpy.mapping.UpdateLayer(df,lyr_wtsd, lyr_sym_wtsd)
 # reset the map extent to that of the state layer file
 df.extent = lyr_sym_state.getExtent()
 # save map doc
 mxd_new.save()
 # export map to PNG file
-arcpy.mapping.ExportToPNG(mxd_new, chr_png_map_new, resolution=300)
+arcpy.mapping.ExportToPNG (mxd_new, chr_png_map_new, resolution=300)
 
 #mxd_base = arcpy.mapping.MapDocument(r'\\U:\TMDL_WR\MidCoast\GIS\Figures\Upper_Yaquina_Maps\Upper_Yaquina_TMDL_BC_Automated_V2.mxd')
 
